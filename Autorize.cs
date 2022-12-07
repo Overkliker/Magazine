7af3bed9e5902abd713d96d8370b8872b692c4bb
Danil <danilahreno@yandex.ru>
2022-12-07 22:16:40 +0300

    TestingPasswordForm

--- Autorize.cs
+++ Autorize.cs
@@ -9,8 +9,31 @@ namespace Magazine
     internal class Autorize
     {
         public static int user;
+
+        public static void PasswordInput(string pass)
+        {
+            /*string password = pass, inpt = string.Empty;
+            while (password != inpt)
+            {
+                Console.Write("Введите пароль для работы с файлами Windows: ");
+                inpt = string.Empty;
+                while (true)
+                {
+                    var key = Console.ReadKey(true);//не отображаем клавишу - true
+
+                    if (key.Key == ConsoleKey.Enter) break; //enter - выходим из цикла
+
+                    Console.Write("*");//рисуем звезду вместо нее
+                    inpt += key.KeyChar; //копим в пароль символы
+                }
+                Console.WriteLine();
+            }*/
+        }
         public static void AdminAut()
         {
+            Console.WriteLine();
+            string name= Console.ReadLine();
+            string password = Console.ReadLine();
 
         }
 
--- Program.cs
+++ Program.cs
@@ -8,12 +8,30 @@ namespace ScorePech
 
         static void Main(string[] argrs)
         {
-            while (true)
+            /*while (true)
             {
                 Console.WriteLine("Выберите под каким пользователем вы хотите зайти:\n" +
                     "1 - Администратор; 2 - Менеджер персонала; 3 - Склад-менеджер; 4 - Кассир; 5 - Бухгалтер");
                 int userInSpase = Convert.ToInt32(Console.ReadLine());
+            }*/
+            string password = "НИКИТА1257", inpt = string.Empty;
+            while (password != inpt)
+            {
+                Console.Write("Введите пароль для работы с файлами Windows: ");
+                inpt = string.Empty;
+                while (true)
+                {
+                    var key = Console.ReadKey(true);//не отображаем клавишу - true
+
+                    if (key.Key == ConsoleKey.Enter) break; //enter - выходим из цикла
+
+                    Console.Write("*");//рисуем звезду вместо нее
+                    inpt += key.KeyChar; //копим в пароль символы
+                }
+                Console.WriteLine();
             }
+            Console.Write("Допуск получен!");
+            Console.ReadKey();
         }
     }
 }
